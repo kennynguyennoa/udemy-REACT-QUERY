7dff846ac8c9a0d550a7479f71b02cad35adebc5
Kenny Nguyen <kenny.nguyen@spk.no>
2023-02-07 15:50:44 +0100

    Added mutation

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,35 +1,34 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 async function fetchComments(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
   return response.json();
 }
 
 async function deletePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "DELETE" }
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/postId/${postId}`, {
+    method: "DELETE",
+  });
   return response.json();
 }
 
 async function updatePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/postId/${postId}`, {
+    method: "PATCH",
+    data: { title: "REACT QUERY FOREVER!!!!" },
+  });
   return response.json();
 }
 
 export function PostDetail({ post }) {
   // replace with useQuery
   //the last parameter in the arrays acts likes its own dependency
-  const { data, isLoading, isError, error } = useQuery(
-    ["comments", post.id],
-    () => fetchComments(post.id)
+  const { data, isLoading, isError, error } = useQuery(["comments", post.id], () =>
+    fetchComments(post.id)
   );
+
+  const deleteMutation = useMutation((postId) => deletePost(postId));
+  const updateMutation = useMutation((postId) => updatePost(postId));
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>{error.toString()}</div>;
@@ -37,7 +36,14 @@ export function PostDetail({ post }) {
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      {deleteMutation.isLoading && <div>Updating...</div>}
+      {deleteMutation.isError && <div>Error...</div>}
+      {deleteMutation.isSuccess && <div>success!</div>}
+      <button onClick={() => updateMutation.mutate(post.id)}>Update title</button>
+      {updateMutation.isLoading && <div>Updating...</div>}
+      {updateMutation.isError && <div>Error...</div>}
+      {updateMutation.isSuccess && <div>success!</div>}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
@@ -15,12 +15,22 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+
+      queryClient.prefetchQuery(["posts", nextPage], () => fetchPosts(nextPage));
+    }
+  }, [currentPage, queryClient]);
+
   const { data, isError, error, isLoading } = useQuery(
     ["posts", currentPage],
     () => fetchPosts(currentPage),
     {
       staleTime: 2000,
+      keepPreviousData: true,
     }
   );
 
@@ -33,17 +43,13 @@ export function Posts() {
       <ul>
         {data.map((post) => (
           <>
-            <li
-              key={post.id}
-              className="post-title"
-              onClick={() => setSelectedPost(post)}
-            >
+            <li key={post.id} className='post-title' onClick={() => setSelectedPost(post)}>
               {post.title}
             </li>
           </>
         ))}
       </ul>
-      <div className="pages">
+      <div className='pages'>
         <button
           disabled={currentPage <= 1}
           onClick={() => {
